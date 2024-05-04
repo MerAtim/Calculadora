7f08b72db6a88e14f098eab59dce42d487031acc
MARIA MERCEDES GUERRA <127444671+MerAtim@users.noreply.github.com>
2024-05-04 00:28:45 -0300

    Se quita bootstrap

--- script.js
+++ script.js
@@ -6,15 +6,19 @@ function calcular() {
     if (!isNaN(valor1) && !isNaN(valor2)) {
         var resultado = document.getElementById("resultado")
         resultado.innerHTML = nombre + ", los resultados son:<br>"
-        resultado.innerHTML += "Suma: " + (valor1 + valor2) + "<br>"
-        resultado.innerHTML += "Resta: " + (valor1 - valor2) + "<br>"
-        resultado.innerHTML += "Multiplicación: " + (valor1 * valor2) + "<br>"
+        resultado.innerHTML += diver("Suma: ") + (valor1 + valor2) + "<br>"
+        resultado.innerHTML += diver("Resta: ") + (valor1 - valor2) + "<br>"
+        resultado.innerHTML += diver("Multiplicación: ") + (valor1 * valor2) + "<br>"
         if (valor2 !== 0) {
-            resultado.innerHTML += "División: " + (valor1 / valor2) + "<br>"
+            resultado.innerHTML += diver("División: ") + (valor1 / valor2) + "<br>"
         } else {
-            resultado.innerHTML += "División: No se puede dividir por cero<br>"
+            resultado.innerHTML += diver("División: ")+ "No se puede dividir por cero<br>"
         }
     } else {
         alert("Por favor, ingrese valores numéricos válidos.")
     }
+}
+
+function diver(str) {
+    return '<span class="tag">' + str +'</span>';
 }
